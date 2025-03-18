6cc63c9a7adaa2b2508ad946b7f9670c02660cd7
stipot <connect@stipot.com>
2025-03-18 11:06:05 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+uv init manimations
+uv add manim
+$env:Path = "C:\Users\inimatic\.local\bin;$env:Path"
+./.venv/scripts/activate.bat
+./.venv/scripts/pip install importlib_metadata
+uv run manim checkhealth 
+uv run manimce -pql main.py CreateCircle
---
+++ main.py
@@ -0,0 +1,15 @@
+from manim import *
+
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+def main():
+    print("Hello from manimations!")
+
+
+if __name__ == "__main__":
+    main()
